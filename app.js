1b1cd87cd9c6f06b8bdbf26ebf0f2781c49a4e8d
syxme <syxme@mail.ru>
2016-06-17 16:43:00 +0300

    новоя версия сайта

--- app.js
+++ app.js
@@ -5,21 +5,57 @@ var express		= require('express'),
 	fs			= require("fs"),
 	async	 	= require("async"),
 	bodyParser	= require('body-parser');
-	login_form	= '';
 	mail = '';
+	
 const root = __dirname+"/";
 var nodemailer = require('nodemailer');
+var TPLS = {};
 
+var fname = function(x){return x.substring(x.lastIndexOf('/')+1,x.length);}
+var hname = function(x){var t = x.substring(x.indexOf('/')+1,x.length);t = t.substring(t.length-5,0);return t.replace('/',"_");}
+var fform = function(x){var name = x.substring(x.lastIndexOf('/')+1,x.length);return name.substring(name.lastIndexOf(".")+1,name.length);}
+
+var walk = function(dir, done) { 
+  var results = [];
+  fs.readdir(dir, function(err, list) {
+    if (err) return done(err);
+    var pending = list.length;
+    if (!pending) return done(null, results);
+    list.forEach(function(file) {
+      file = dir + '/' + file;
+      fs.stat(file, function(err, stat) {
+        if (stat && stat.isDirectory()) {
+          walk(file, function(err, res) {
+            results = results.concat(res);
+            if (!--pending) done(null, results);
+          });
+        } else {
+          results.push(file);
+          if (!--pending) done(null, results);
+        }
+      });
+    });
+  });
+};
+
+walk("public", function(err, file) {
+	Object.keys(file).forEach(function(number){
+		if (fform(fname(file[number]))=="html"){
+			TPLS[hname(fname(file[number]))] = fs.readFileSync(root+file[number], 'utf8');
+		}
+	});
+});
 // create reusable transporter object using the default SMTP transport
-var transporter = nodemailer.createTransport('smtps://djsystem6@mail.ru:XX@smtp.mail.ru');
+var transporter = nodemailer.createTransport('smtps://djsystem6@mail.ru:wBiKy2Deee@smtp.mail.ru');
 transporter.transporter.options.port = 465;
+
 function sendMail (phone,name,problem) {
 	var mailOptions = {
 	    from: '"Сервис Новый заказ" <djsystem6@mail.ru>', // sender address
 	    to: 'systemxmedias@gmail.com', // list of receivers
 	    subject: name+' '+ phone+'✔', // Subject line
-	    text: name+' '+ phone+' '+problem+' 🐴', // plaintext body
-	    html:  name+' '+ phone+' '+problem+' 🐴' // html body
+	    text: name+'|'+ phone+'|'+problem+' 🐴', // plaintext body
+	    html:  name+'|'+ phone+'|'+problem+' 🐴' // html body
 	};
 
 	transporter.sendMail(mailOptions, function(error, info){
@@ -37,31 +73,51 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
-fs.readFile(root+'public/index.html', 'utf8', function (err,data) {
-	if (err) {
-		return console.log('error read file tpl');
-	}else{
-		login_form = data;
-	}
-});
 
 var port =process.env.OPENSHIFT_NODEJS_PORT || 8080 ;
 var ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
-var index = function(req,res){res.send(login_form);}
 
 
-app.get('/', function(req,res){res.send(login_form);});
+
+
+
 app.get('/goodsend', function(req,res){
 	res.send(login_form.replace('<!--XDX-->','<div  id="messages" onClick="hide(\'messages\')"class="message">Ваша Заявка прянята<br>Ожидайте звонка</div>'));
 });
 
-app.post('/', function(req,res){
+app.post('/sendact', function(req,res){
 	console.log(req.body);
-	sendMail(req.body.phone,req.body.author,req.body.text);
-	res.redirect('/goodsend');
+	sendMail(req.body['your-tel'],req.body['your-name'],req.body['your-time']);
+	res.redirect('/');
 });
 
+// [ 'contacts',
+//   'index',
+//   'nastrojka-interneta',
+//   'nastrojka-kompyuterov',
+//   'onas',
+//   'price',
+//   'remont-kompyuterov',
+//   'skoraya-kompyuternaya-pomoshh',
+//   'uslugi',
+//   'ustanovka-windows-7',
+//   'vosstanovlenie-dannyx-na-domu' ]
+
+
+//GET
+app.get('/', function(req,res){res.send(TPLS['index']);});
+app.get('/onas', function(req,res){res.send(TPLS['onas']);});
+app.get('/uslugi', function(req,res){res.send(TPLS['uslugi']);});
+app.get('/prajs', function(req,res){res.send(TPLS['price']);});
+app.get('/kontakty', function(req,res){res.send(TPLS['contacts']);});
+
+app.get('/remontondom', function(req,res){res.send(TPLS['remont-kompyuterov']);});
+app.get('/vostanovl', function(req,res){res.send(TPLS['vosstanovlenie-dannyx-na-domu']);});
+app.get('/setting', function(req,res){res.send(TPLS['nastrojka-kompyuterov']);});
+app.get('/needhelp', function(req,res){res.send(TPLS['skoraya-kompyuternaya-pomoshh']);});
+app.get('/install', function(req,res){res.send(TPLS['ustanovka-windows-7']);});
+app.get('/settingprint', function(req,res){res.send(TPLS['nastrojka-interneta']);});
 
 // app.get('*', function(req, res) {
 //     res.redirect('http://google.com');
