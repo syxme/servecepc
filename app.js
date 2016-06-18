63724b190453900a89222ffb4d06b7d93ac3cebe
syxme <syxme@mail.ru>
2016-06-18 21:02:28 +0300

    Установил шаблонизатор и поправил шаблоны

--- app.js
+++ app.js
@@ -1,72 +1,37 @@
 var express		= require('express'),
-	//swig		= require('swig'),
 	app			= express(),
 	server		= require('http').createServer(app),
-	fs			= require("fs"),
-	async	 	= require("async"),
+	nodemailer	= require('nodemailer'),
+	hbs			= require('express-handlebars'),
+	port		= process.env.OPENSHIFT_NODEJS_PORT || 8080 ,
+	ip			= process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1",
 	bodyParser	= require('body-parser');
-	mail = '';
-	
+
 const root = __dirname+"/";
-var nodemailer = require('nodemailer');
-var TPLS = {};
+const aci = 'current-menu-item';
 
-var fname = function(x){return x.substring(x.lastIndexOf('/')+1,x.length);}
-var hname = function(x){var t = x.substring(x.indexOf('/')+1,x.length);t = t.substring(t.length-5,0);return t.replace('/',"_");}
-var fform = function(x){var name = x.substring(x.lastIndexOf('/')+1,x.length);return name.substring(name.lastIndexOf(".")+1,name.length);}
-
-var walk = function(dir, done) { 
-  var results = [];
-  fs.readdir(dir, function(err, list) {
-    if (err) return done(err);
-    var pending = list.length;
-    if (!pending) return done(null, results);
-    list.forEach(function(file) {
-      file = dir + '/' + file;
-      fs.stat(file, function(err, stat) {
-        if (stat && stat.isDirectory()) {
-          walk(file, function(err, res) {
-            results = results.concat(res);
-            if (!--pending) done(null, results);
-          });
-        } else {
-          results.push(file);
-          if (!--pending) done(null, results);
-        }
-      });
-    });
-  });
-};
-
-walk("public", function(err, file) {
-	Object.keys(file).forEach(function(number){
-		if (fform(fname(file[number]))=="html"){
-			TPLS[hname(fname(file[number]))] = fs.readFileSync(root+file[number], 'utf8');
-		}
-	});
-});
-// create reusable transporter object using the default SMTP transport
 var transporter = nodemailer.createTransport('smtps://djsystem6@mail.ru:wBiKy2Deee@smtp.mail.ru');
 transporter.transporter.options.port = 465;
 
 function sendMail (phone,name,problem) {
 	var mailOptions = {
-	    from: '"Сервис Новый заказ" <djsystem6@mail.ru>', // sender address
-	    to: 'systemxmedias@gmail.com', // list of receivers
-	    subject: name+' '+ phone+'✔', // Subject line
-	    text: name+'|'+ phone+'|'+problem+' 🐴', // plaintext body
-	    html:  name+'|'+ phone+'|'+problem+' 🐴' // html body
+	    from: '"Сервис Новый заказ" <djsystem6@mail.ru>', 
+	    to: 'systemxmedias@gmail.com', 
+	    subject: name+' '+ phone+'✔', 
+	    text: name+'|'+ phone+'|'+problem+' 🐴', 
+	    html:  name+'|'+ phone+'|'+problem+' 🐴' 
 	};
 
 	transporter.sendMail(mailOptions, function(error, info){
-	    if(error){
-	        return console.log(error);
-	    }
-	    console.log('Message sent: ' + info.response);
+		if(error){
+			return console.log(error);
+		}
+		console.log('Message sent: ' + info.response);
 	});
 }
 
-
+app.engine('hbs', hbs({defaultLayout:'main.hbs'}));
+app.set('view engine', 'hbs');
 app.use(express.static(root+'public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -74,54 +39,28 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
-var port =process.env.OPENSHIFT_NODEJS_PORT || 8080 ;
-var ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-
-
-
-
-
-
-app.get('/goodsend', function(req,res){
-	res.send(login_form.replace('<!--XDX-->','<div  id="messages" onClick="hide(\'messages\')"class="message">Ваша Заявка прянята<br>Ожидайте звонка</div>'));
-});
-
 app.post('/sendact', function(req,res){
 	console.log(req.body);
 	sendMail(req.body['your-tel'],req.body['your-name'],req.body['your-time']);
 	res.redirect('/');
 });
 
-// [ 'contacts',
-//   'index',
-//   'nastrojka-interneta',
-//   'nastrojka-kompyuterov',
-//   'onas',
-//   'price',
-//   'remont-kompyuterov',
-//   'skoraya-kompyuternaya-pomoshh',
-//   'uslugi',
-//   'ustanovka-windows-7',
-//   'vosstanovlenie-dannyx-na-domu' ]
 
 
-//GET
-app.get('/', function(req,res){res.send(TPLS['index']);});
-app.get('/onas', function(req,res){res.send(TPLS['onas']);});
-app.get('/uslugi', function(req,res){res.send(TPLS['uslugi']);});
-app.get('/prajs', function(req,res){res.send(TPLS['price']);});
-app.get('/kontakty', function(req,res){res.send(TPLS['contacts']);});
 
-app.get('/remontondom', function(req,res){res.send(TPLS['remont-kompyuterov']);});
-app.get('/vostanovl', function(req,res){res.send(TPLS['vosstanovlenie-dannyx-na-domu']);});
-app.get('/setting', function(req,res){res.send(TPLS['nastrojka-kompyuterov']);});
-app.get('/needhelp', function(req,res){res.send(TPLS['skoraya-kompyuternaya-pomoshh']);});
-app.get('/install', function(req,res){res.send(TPLS['ustanovka-windows-7']);});
-app.get('/settingprint', function(req,res){res.send(TPLS['nastrojka-interneta']);});
+// GET
+app.get('/', function(req,res){res.render('index',{index:aci,slider:true});});
+app.get('/onas', function(req,res){res.render('onas',{onas:aci});});
+app.get('/uslugi', function(req,res){res.render('uslugi',{uslugi:aci});});
+app.get('/prajs', function(req,res){res.render('price',{price:aci});});
+app.get('/kontakty', function(req,res){res.render('contacts',{contacts:aci});});
 
-// app.get('*', function(req, res) {
-//     res.redirect('http://google.com');
-// });
+app.get('/remontondom', function(req,res){res.render('remont-kompyuterov',{rk:aci});});
+app.get('/vostanovl', function(req,res){res.render('vosstanovlenie-dannyx-na-domu',{vd:aci});});
+app.get('/setting', function(req,res){res.render('nastrojka-kompyuterov',{nk:aci});});
+app.get('/needhelp', function(req,res){res.render('skoraya-kompyuternaya-pomoshh',{help:aci});});
+app.get('/install', function(req,res){res.render('ustanovka-windows-7',{ust:aci});});
+app.get('/settingprint', function(req,res){res.render('nastrojka-interneta',{nast:aci});});
 
 
 server.listen(port, ip);
